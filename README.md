575915ad62e2a9874ae9c5fd4c8fa431c52118f3
相得益张 <zhj778930960@outlook.com>
2022-01-11 19:00:26 +0800

    <feat >

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 2. react-router的封装，如何封装成菜单模式
 3. react 各种全局数据持久库的实践
 4. ts 的使用
+
+https://zhuanlan.zhihu.com/p/407003986
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {  Link, Route, Routes } from 'react-router-dom'
+import Login from './pages/login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}123123防守打法
@@ -36,8 +38,14 @@ function App() {
           >
             Vite Docs
           </a>
-        </p>
-      </header>
+        </p> */}
+      {/* </header> */}
+
+   <Link to={'/first'}>123</Link>
+   
+      <Routes>
+        <Route path='/first' element={<Login></Login>}></Route>
+      </Routes>
     </>
   )
 }
